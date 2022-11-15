a1f5bc2bd7f845c9c83cda1a29f0d85cdc313493
Chukcy <chukcyihub.inc@gmail.com>
2022-11-15 11:44:25 +0100

    Before implementing dropdown

--- lib/widgets/home_widget.dart
+++ lib/widgets/home_widget.dart
@@ -10,9 +10,67 @@ class HomeWidget extends StatefulWidget {
 class _HomeWidgetState extends State<HomeWidget> {
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Center(
-        child: Text("Home widget"),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20.0,
+            ),
+            // This is housing the search input for contacts
+            const Padding(
+              padding: EdgeInsets.only(
+                left: 25.0,
+                right: 25.0,
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  suffixIcon: Icon(Icons.search_rounded),
+                  hintText: "search contacts",
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            // This will be housing a list of contacts
+            Expanded(
+              child: ListView.builder(
+                  itemCount: 80,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      contentPadding:
+                          const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 5.0),
+                      leading: Image.asset(
+                        "assets/images/add-user.png",
+                        height: 45.0,
+                      ),
+                      title: const Text(
+                        "Ayra Starr",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                      ),
+                      subtitle: const Text(
+                        "+234 702 688 1411",
+                      ),
+                      trailing: const Icon(Icons.arrow_forward_ios_rounded),
+                      // trailing: DropdownButton(
+                      //   items: const [
+                      //     DropdownMenuItem(
+                      //       child: Text("View Contacts"),
+                      //     ),
+                      //     DropdownMenuItem(
+                      //       child: Text("Add favorite"),
+                      //     ),
+                      //   ],
+                      //   onChanged: (value) {},
+                      // ),
+                    );
+                  }),
+            ),
+          ],
+        ),
       ),
     );
   }
